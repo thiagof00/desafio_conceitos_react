aa8cc8a13ca83bd2bce3c822ccadc6854e838c82
thiagof00 <thiagoribeiro0302@gmail.com>
2020-08-21 17:20:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from "react";
+
+import api from './services/api.js'
+
+import "./styles.css";
+
+function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(()=>{
+
+    api.get("/repositories").then(response => setRepositories(response.data))
+
+  }, [])
+
+  async function handleAddRepository() {
+    
+   const response = await api.post("/repositories", {
+        title: `Novo projeto ${Date.now()}`,
+        url: "thiago.com",
+        techs: ["Angular", "Postgress", "React"]
+    })
+
+    const repository = response.data 
+    
+    setRepositories([...repositories, repository])
+
+  }
+
+  async function handleRemoveRepository(id) {
+    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(repository => repository.id != id))
+   
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        
+        {repositories.map(repository =><li key={repository.id}>
+          
+          {repository.title}
+        <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>)}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
